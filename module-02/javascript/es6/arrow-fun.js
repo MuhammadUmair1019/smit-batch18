fcda493cdda0a11736c4b85a4cf7eabf29a4e61b
Muhammad Umair <muhammad.umair1019@gmail.com>
2026-03-30 15:21:08 +0500

    map and filter method

--- module-02/javascript/es6/arrow-fun.js
+++ module-02/javascript/es6/arrow-fun.js
@@ -1,40 +1,80 @@
-// Arrow Functions in JavaScript
+// let calc = (x, y, f) => f(x, y);
 
-// Definition:
-// An arrow function is a shorter way to write functions using =>
+// let sum = (x, y) => x + y;
+// let sub = (x, y) => x - y;
+// let mul = (x, y) => x * y;
+// let div = (x, y) => x / y;
+// let max = (x, y) => (x > y ? x : y);
 
-// Returning an Object
-let z = () => ({ id: 101, name: "Ali" });
-console.log(z());
+// let result = calc(2, 1, max);
+// console.log(result);
 
-// Short Form (Implicit Return)
-let sum = (a, b) => a + b;
-let result1 = sum(3, 3);
-console.log(result1);
+// -----------------------------------------------
+// // variations -> left side arrow
+// (x, y) => x + y;
+// (x) => x + 1;
+// () => console.log("Hello");
+// _ => console.log("Hello");
 
-//Block Body (Explicit Return)
-let add = (a, b) => {
-    return a + b;
-};
-console.log(add(2, 3));
+// // variations -> right side arrow
+// (x, y) => x + y;
+// (x, y) => {
+//   x = x + 1;
+//   y = y + 1;
+//   return x + y;
+// };
+// () => [2, 3, 4];
+// () => ({id: 101, firstName: 'Ali'})
 
-// No Parameters
-let greet = () => console.log("Hello");
-greet();
+// arrow function
+// let sum = (x, y) => x + y;
+// let ans = sum(2, 3);
+// console.log(ans)
 
-// Return Value Check
-let check = () => console.log("Hello");
-let result2 = check();
-console.log(result2); // undefined
+// function sum(x, y) {
+//     return x + y;
+// }
 
+// let ans = sum(2, 4);
+// console.log(ans)
 
-// Normal Function (Traditional Way)
+// ----------------------------------------------------------------
+// // Arrow Functions in JavaScript
 
-// Definition:
-// A normal function uses the 'function' keyword
+// // Definition:
+// // An arrow function is a shorter way to write functions using =>
 
-function y() {
-    console.log("Hello!");
-}
+// // Returning an Object
+// let z = () => ({ id: 101, name: "Ali" });
+// console.log(z());
 
-y();
+// // Short Form (Implicit Return)
+// let sum = (a, b) => a + b;
+// let result1 = sum(3, 3);
+// console.log(result1);
+
+// //Block Body (Explicit Return)
+// let add = (a, b) => {
+//     return a + b;
+// };
+// console.log(add(2, 3));
+
+// // No Parameters
+// let greet = () => console.log("Hello");
+// greet();
+
+// // Return Value Check
+// let check = () => console.log("Hello");
+// let result2 = check();
+// console.log(result2); // undefined
+
+// // Normal Function (Traditional Way)
+
+// // Definition:
+// // A normal function uses the 'function' keyword
+
+// function y() {
+//     console.log("Hello!");
+// }
+
+// y();
--- module-02/javascript/es6/map-filter.js
+++ module-02/javascript/es6/map-filter.js
@@ -1,33 +1,78 @@
-// Array map() Method in JavaScript
+let marks = [10, 20, 30];
+let filterdArr = marks.filter((m) => m < 30).map((m) => m + 10);
 
-// Definition:
-// map() creates a new array by applying a function to each element of the original array
+console.log(filterdArr);
+// let filterdArr = marks.filter(m => m < 30); // []
 
-let marks = [30, 40, 50];
+// filterdArr = filterdArr.map(m => m + 10)
 
-//  Using arrow function with ternary operator
-let modifiedMarks = marks.map((m) => (m < 40 ? m + 10 : m));
+// console.log(filterdArr);
 
-console.log(marks);
-console.log(modifiedMarks);
-
-// Using arrow function with if-else
-// let modifiedMarks = marks.map((m) => {
-//     if (m < 40) {
-//         return m + 10;
+// let filterdArr = marks.filter((m) => {
+//     if(m < 30) {
+//         return true
 //     } else {
-//         return m;
+//         return false
 //     }
-// });
+// }); // []
 
+// console.log(filterdArr)
 
-// let modifiedMarks = marks.map((m) => m + 10);
+// any expression
+// marks.map((m) =>m + 10);
 
-//  Traditional loop (without map)
-// let copyArr = [];
-// for (let i = 0; i < marks.length; i++) {
-//     copyArr.push(marks[i] + 10);
-// }
+let copyMarks = marks.map((m) => {
+  if (m < 30) {
+    return m + 10;
+  } else {
+    return m;
+  }
+}); // [20, 30, 40]
 
 // console.log(marks);
-// console.log(copyArr);
+// console.log(copyMarks)
+
+// -------------------------------------------
+// let marks = [10, 20, 30]; // [20, 30, 40]
+// let copyArr = [];
+
+// for(let i=0; i < marks.length; i++) {
+//     copyArr.push(marks[i] + 10)
+// }
+
+// console.log(marks)
+// console.log(copyArr)
+
+// ------------------------------------------
+// // Array map() Method in JavaScript
+
+// // Definition:
+// // map() creates a new array by applying a function to each element of the original array
+
+// let marks = [30, 40, 50];
+
+// //  Using arrow function with ternary operator
+// let modifiedMarks = marks.map((m) => (m < 40 ? m + 10 : m));
+
+// console.log(marks);
+// console.log(modifiedMarks);
+
+// // Using arrow function with if-else
+// // let modifiedMarks = marks.map((m) => {
+// //     if (m < 40) {
+// //         return m + 10;
+// //     } else {
+// //         return m;
+// //     }
+// // });
+
+// // let modifiedMarks = marks.map((m) => m + 10);
+
+// //  Traditional loop (without map)
+// // let copyArr = [];
+// // for (let i = 0; i < marks.length; i++) {
+// //     copyArr.push(marks[i] + 10);
+// // }
+
+// // console.log(marks);
+// // console.log(copyArr);
